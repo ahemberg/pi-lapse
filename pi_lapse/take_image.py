031565b38ca2587664cf6391c46ce4976806f2cd
ahemberg <UfQRemBWB6fHxXtHV3Id>
2018-02-18 12:36:33 +0100

    Modified for consistent timelapses

--- pi_lapse/take_image.py
+++ pi_lapse/take_image.py
@@ -1,11 +1,33 @@
 from time import sleep
-from picamera import PiCamera
+import picamera
 import datetime
+import os
 
 
-camera = PiCamera()
-camera.resolution = (1920, 1080)
-camera.start_preview()
-# Camera warm-up time
-sleep(2)
-camera.capture("/home/alex/tl/" + datetime.datetime.now().strftime("%Y-%m-%d_%H%M%S") + '.jpg')
+def assure_path_exists(path):
+    dir = os.path.dirname(path)
+    print(dir)
+    if not os.path.exists(dir):
+        print('making dir!!')
+        os.makedirs(dir)
+
+
+base_dir = '/home/alex/timelapses/'
+
+now = datetime.datetime.now()
+
+# Create todays directory if it does not exist
+todays_dir = '%s/%s/' % (base_dir, now.strftime("%Y%m%d"))
+assure_path_exists(todays_dir)
+
+with picamera.PiCamera() as camera:
+    camera.resolution = (1920, 1080)
+    camera.framerate = 30
+    sleep(2)
+    camera.shutter_speed = camera.exposure_speed
+    camera.exposure_mode = 'off'
+    g = camera.awb_gains
+    camera.awb_mode = 'off'
+    camera.awb_gains = g
+    camera.capture(todays_dir + now.strftime("%Y-%m-%d_%H%M%S") + '.jpg')
+
