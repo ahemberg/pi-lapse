a48470fc2cdde030f3b8e3bec7656260432dbe3b
ahemberg <UfQRemBWB6fHxXtHV3Id>
2018-02-18 13:39:18 +0100

    Add still_stats=true to reduce cyan vignetting

--- pi_lapse/take_image.py
+++ pi_lapse/take_image.py
@@ -22,12 +22,17 @@ assure_path_exists(todays_dir)
 
 with picamera.PiCamera() as camera:
     camera.resolution = (1920, 1080)
-    camera.framerate = 30
-    sleep(2)
-    camera.shutter_speed = camera.exposure_speed
-    camera.exposure_mode = 'off'
-    g = camera.awb_gains
-    camera.awb_mode = 'off'
-    camera.awb_gains = g
+    camera.exposure_mode = 'auto'
+    camera.still_stats = True
+    #camera.framerate = 30
+    camera.start_preview()
+    sleep(10)
+    #camera.exposure_mode = 'auto'
+    #camera.shutter_speed = camera.exposure_speed
+    #camera.exposure_mode = 'off'
+    #g = camera.awb_gains
+    #camera.awb_mode = 'off'
+    #camera.awb_gains = g
+    #sleep(2)
     camera.capture(todays_dir + now.strftime("%Y-%m-%d_%H%M%S") + '.jpg')
 
